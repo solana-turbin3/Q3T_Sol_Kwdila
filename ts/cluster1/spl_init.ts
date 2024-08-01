945ddf52adbd44f1bb600de6d6bcb524d14a6455
hema <shaswarhema@gmail.com>
2024-08-01 12:12:18 +0300

    finished spl exercises

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,7 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { createMint } from "@solana/spl-token";
+import wallet from "./wallet/wba-wallet.json";
+import { log } from "console";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,10 +11,16 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6
+    );
+    console.log("The mint id is: ", mint.toBase58());
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
